f4f22130ca0a1f7fb00ca7676cc920a3bbfd262e
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-07-17 18:53:39 +0800

    “push”

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.MongoClient.connect(process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 
