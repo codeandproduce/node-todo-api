7ccdc1f3afc3a1b76c657fab9480680da855f56f
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-07-18 15:23:46 +0800

    auth

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -31,9 +33,41 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if(_isBoolean(body.completed) && body.completed){
+    body.completedAt = new Data().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
+});
+
 app.listen(port, () => {
   console.log('Started on port ', port);
 });
+
+
 
 
 // //create an instance of the model
